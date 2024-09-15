17203d3a08dae3f4dd0bab5ecb782597cc8cb53c
filter00 <alienorigin36940@gmail.com>
2024-09-15 13:32:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kingofhell268:3rrk9V7ufyCF9gWz@cluster0.afgwdof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bno815667:ZyZWuWMNOP2ZEciL@cluster0.cyg98.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ANKIT")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
