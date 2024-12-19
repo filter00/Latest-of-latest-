a5dd1d108939a856b1aa25556758c58ebc5e5afa
filter00 <alienorigin36940@gmail.com>
2024-12-19 22:44:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID')
 reqst_channel = environ.get('REQST_CHANNEL')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
+NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://onlyforusetstuffs:JTp66ahxVnHDza2V@cluster0.bqyuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
