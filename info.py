4567c06016bc82c881be571b617a1cbc23c23694
filter00 <alienorigin36940@gmail.com>
2025-03-20 08:22:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,17 +51,17 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'coa09')
 premium = environ.get('PREMIUM_LOGS', '-1001424329361')
 PREMIUM_LOGS = int(premium) if premium and id_pattern.search(premium) else None
 
-IMPORT_JK_SITE = environ.get('IMPORT_JK_SITE', 'shortxlinks.com')
-IMPORT_JK_API = environ.get('IMPORT_JK_API', '615f8ae00dc24cbb5f55fc88e30b99fe8e27ad0a')
+IMPORT_JK_SITE = environ.get('IMPORT_JK_SITE', 'inshorturl.com')
+IMPORT_JK_API = environ.get('IMPORT_JK_API', 'f32e279366f8d1960d32811e1c3fd058db103fc0')
 SHORT_MODE = is_enabled((environ.get("SHORT_MODE","False")), False)
 
 # Others
-IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
+IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/tutorialcoa")
-VERIFY2_URL = environ.get('VERIFY2_URL', "shortxlinks.com")
-VERIFY2_API = environ.get('VERIFY2_API', "615f8ae00dc24cbb5f55fc88e30b99fe8e27ad0a")
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '615f8ae00dc24cbb5f55fc88e30b99fe8e27ad0a')
+VERIFY2_URL = environ.get('VERIFY2_URL', "inshorturl.com")
+VERIFY2_API = environ.get('VERIFY2_API', "f32e279366f8d1960d32811e1c3fd058db103fc0")
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'inshorturl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'f32e279366f8d1960d32811e1c3fd058db103fc0')
 IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), False)
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "8")
